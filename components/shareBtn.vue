a08c2723e79488519342ea7766e4a997ff202c17
Thiago Carvalho <thiagocarvalho.ads@gmail.com>
2020-01-12 11:24:41 -0300

    Merged remote master

---
+++ components/shareBtn.vue
@@ -0,0 +1,101 @@
+<template>
+  <v-layout>
+    <div v-if="isOpen" class="share-container">
+      <social-sharing
+        :url="url"
+        :title="title"
+        :description="description"
+        :hashtags="hashtags"
+        :twitter-user="twitterUser"
+        @open="isOpen = !isOpen"
+        inline-template
+      >
+        <div class="icons">
+          <network network="facebook">
+            <v-icon color="#fff" size="30">
+              mdi-facebook
+            </v-icon>
+          </network>
+          <network network="twitter">
+            <v-icon color="#fff" size="30">
+              mdi-twitter
+            </v-icon>
+          </network>
+          <network network="linkedin">
+            <v-icon color="#fff" size="30">
+              mdi-linkedin
+            </v-icon>
+          </network>
+        </div>
+      </social-sharing>
+    </div>
+    <v-btn
+      v-if="!isOpen"
+      :color="color"
+      @click="isOpen = !isOpen"
+      class="ma-2"
+      tile
+      large
+      icon
+    >
+      <v-icon>
+        mdi-share-variant
+      </v-icon>
+    </v-btn>
+  </v-layout>
+</template>
+
+<script>
+import SocialSharing from 'vue-social-sharing'
+
+export default {
+  components: {
+    SocialSharing
+  },
+
+  props: {
+    color: {
+      default: '#CCC'
+    },
+    url: String,
+    title: String,
+    description: String,
+    hashtags: {
+      default: 'MissaoNewSchoolApp'
+    },
+    twitterUser: {
+      default: 'NewSchoolApp'
+    }
+  },
+
+  data () {
+    return {
+      isOpen: false
+    }
+  }
+}
+</script>
+
+<style>
+.share-container {
+  width: 90px;
+  height: 35px;
+  background: #60C;
+}
+
+.icons {
+  display: flex;
+}
+
+.share-container .icons network v-icons {
+  max-width: 5px;
+}
+
+.facebook {
+  max-width: 5px;
+}
+
+.ma-2 {
+  padding-left: 55px;
+}
+</style>
---
+++ pages/certificates.vue
@@ -0,0 +1,184 @@
+<template>
+  <v-container class="main">
+    <div>
+      <h1 class="text-purple">
+        Meus certificados
+      </h1>
+    </div>
+    <div class="cards-box">
+      <div class="image-background">
+        <img src="../assets/medalha-image.svg" alt="Imagem medalhas">
+      </div>
+      <div class="footer">
+        <div class="title-socias">
+          <strong class="title-black">Matemática na prática</strong>
+          <!-- <div class="icon-share">
+          <img src="../assets/facebook.png" alt="">
+              <img src="../assets/google.png" alt="">
+              <img src="../assets/instagram.png" alt="">
+            </div>
+            <button type="button" class="comp" @click="share">
+              <img class="compartilhe" src="../assets/icon-social-shape_24px.svg" alt="">
+            </button>
+          </div> -->
+          <div class="icon-share">
+            <shareBtn />
+          </div>
+        </div>
+        <p>Thiago Carvalho</p>
+        <div class="cards-box">
+          <div class="image-background">
+            <img src="../assets/medalha-image.svg" alt="Imagem medalhas">
+          </div>
+          <div class="footer">
+            <div class="title-socias">
+              <strong class="title-black">Matemática na prática</strong>
+              <!-- <div class="icon-share">
+              <img src="../assets/facebook.png" alt="">
+              <img src="../assets/google.png" alt="">
+              <img src="../assets/instagram.png" alt="">
+            </div>
+            <button type="button" class="comp" @click="share">
+              <img class="compartilhe" src="../assets/icon-social-shape_24px.svg" alt="">
+            </button> -->
+            </div>
+            <p>Thiago Carvalho</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </v-container>
+</template>
+<script>
+import shareBtn from '../components/shareBtn'
+
+export default {
+  components: {
+    shareBtn
+  }
+  // data: () => ({
+  //   certifactes: []
+  // }),
+  // mounted () {
+  //   certificateService.findAllCertificatesByUser('48310300-e9d2-4d9c-929e-ed626126673e')
+  //     .then((certificates) => {
+  //       this.certificates = certificates.data
+  //       console.log(certificates)
+  //     }).catch(error => console.log(error))
+  // },
+  // methods: {
+  //   backgroundClass (certificateBackgroundName) {
+  //     return `background-image: url(/_nuxt/assets/${certificateBackgroundName})`
+  //   },
+  //   share () {
+  //     alert('Try-out')
+  //   }
+  // }
+}
+</script>
+<style>
+  .main {
+    margin-top: 30px;
+    width: 375px;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+  }
+
+  button {
+    /* display: none; */
+    cursor: pointer !important;
+  }
+
+  .cards-box {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  .card {
+    margin-top: 10px;
+  }
+  .text-purple {
+    font-family: Montserrat;
+    color: #6600CC;
+    font-size: 16px;
+    text-transform: uppercase;
+    padding-left: 34px;
+    margin-bottom: 10px;
+  }
+  .image-background {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 200px;
+    height: 115px;
+    background: #6600cc url("../assets/matematica.svg");
+    /* background: #6600cc url("../assets/fotografia.svg");
+    background: #6600cc url("../assets/cenografia.svg") */
+  }
+
+  .footer {
+    width: 200px;
+    padding-top: 10px;
+    line-height: 15px;
+    text-align: center;
+
+    font-family: Montserrat;
+    font-style: normal;
+    font-weight: 500;
+  }
+  .title-black {
+    font-family: Montserrat;
+    font-style: normal;
+    font-weight: 600;
+    font-size: 12px;
+    line-height: 15px;
+    display: flex;
+    color: #1A1A1A;
+    padding-left: 2px;
+
+    align-items: center;
+    text-align: center;
+
+  }
+
+  .footer p {
+    font-family: Montserrat;
+    font-style: normal;
+    font-weight: 300;
+    font-size: 10px;
+    line-height: 12px;
+    padding-left: 2px;
+
+    color: #1A1A1A;
+    margin-top: 10px;
+    display: flex;
+    align-items: center;
+
+  }
+  .title-socias {
+    font-family: Montserrat;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .compartilhe {
+    max-width: 24px;
+  }
+
+  .icon-share{
+    position: absolute;
+    margin: 0 0 0 110px
+    /* display: flex;
+    align-items: center;
+    justify-content: space-around; */
+
+    /* display: none;
+    width: 90px;
+    height: 35px; */
+    /* background: #6600CC; */
+    /* box-shadow: 2px 4px 4px rgba(0, 0, 0, 0.25); */
+  }
+</style>
